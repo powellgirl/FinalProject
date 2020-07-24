77b19064df15e1543002b38cd102a9f3b6891140
Lauren Tolley <powellgirl360@gmail.com>
2020-07-24 11:16:18 -0700

    added items

---
+++ items.js
@@ -0,0 +1,23 @@
+export const items = [
+    {
+        "title": "Glider",
+        "value": "100,000 coins",
+        "nation_origin": "Air Nomad",
+        "time_period": "16 BA",
+        "picture": "https://www.blendswap.com/blend_previews/15794/0/0", 
+    },
+    {
+        "title": "Air Nomad Bead Necklace",
+        "value": "500,000 coins",
+        "nation_origin": "Air Nomad",
+        "time_period": "500 BA",
+        "picture": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/07ebe858-89b3-4fbd-9393-b802903da8e5/d7yfoey-8dc8ec9a-874e-46cd-b3bb-2abac0e0e4a5.jpg/v1/fill/w_1024,h_1024,q_75,strp/avatar_aang_s_necklace_by_domini007_d7yfoey-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDI0IiwicGF0aCI6IlwvZlwvMDdlYmU4NTgtODliMy00ZmJkLTkzOTMtYjgwMjkwM2RhOGU1XC9kN3lmb2V5LThkYzhlYzlhLTg3NGUtNDZjZC1iM2JiLTJhYmFjMGUwZTRhNS5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.w9mhOMQd70qEbOHeP5q6bqw5MFhp-Nn_VH14WBlPjYY", 
+    },
+    {
+        "title": "Water Scrolls",
+        "value": "250,000 coins",
+        "nation_origin": "Water Tribe",
+        "time_period": "165 BA",
+        "picture": "https://i.etsystatic.com/8984785/r/il/bab250/736878416/il_570xN.736878416_gksz.jpg", 
+    },
+]
